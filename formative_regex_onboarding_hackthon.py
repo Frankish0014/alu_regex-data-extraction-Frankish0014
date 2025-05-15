4058d1f0ea50946a31e95213c7ab0c65f2f71be6
Frankish0014 <f.ishimwe@alustudent.com>
2025-05-15 22:23:35 +0200

    This criterion is linked to a Learning OutcomeOutput Presentation

--- formative_regex_onboarding_hackthon.py
+++ formative_regex_onboarding_hackthon.py
@@ -6,4 +6,27 @@ URLs:
 
 ^https:\/\/[a-zA-Z0-9.-].[a-zA-Z0-9]{2,}.(\/[^\s]*)?
 
+Phone numbers (Various formate)
+
+^(\(\d{3}\)|\d{3})[-. ]\d{3}[-. ]\d{4}$
+
+
+Credit Card numbers
+^\d{4}[-. ]\d{4}[-. ]\d{4}[-. ]\d{4}$
+
+Time
+
+^([01]?[0-9]|2[0-3]):[0-5][0-9]$
+
+
+Html Tags
+<[^>]+>
+
+HashTags
+#\w+
+
+Currency amounts
+
+^[^\d\s-]?\d{1,3}(,\d{3})*(\.\d{2})?$|^[^\d\s-]?\d+(\.\d{2})?$
+
 
